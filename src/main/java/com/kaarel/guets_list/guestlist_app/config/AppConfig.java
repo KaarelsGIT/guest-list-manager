37f05454dee0c3f9f98531cbab692f2feabdf13f
kaarelkattai <kaarel.kattai@gmail.com>
2025-05-24 11:45:57 +0300

    minor update AppConfig.java

--- src/main/java/com/kaarel/guets_list/guestlist_app/config/AppConfig.java
+++ src/main/java/com/kaarel/guets_list/guestlist_app/config/AppConfig.java
@@ -10,9 +10,7 @@ public class AppConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://localhost:4200"
-                )
+                .allowedOrigins("http://localhost:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
